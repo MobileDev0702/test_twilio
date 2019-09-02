066173ef764b5c8c8041d807ac654ce8b31d06e8
freshcoderchan <freshcoderjack@outlook.com>
2019-09-02 11:12:52 -0400

    make sending sms by twilio

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
 // routes/index.js
 
+require('dotenv').config()
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const querystring = require('querystring');
 
 router.post('/send', function (req, res, next) {
-	res.send('Hello');
+	axios.post(process.env.URL, querystring.stringify({
+			From: process.env.FROM,
+			To: process.env.TO,
+			Body: process.env.BODY,
+		}), {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			},
+			// withCredentials: true,
+			auth: {
+				username: process.env.USER_NAME,
+				password: process.env.PASS_WORD
+			},
+			// responseType: 'json'
+	})
+	.then((response) => {
+		console.log(typeof(response));
+		// res.send(JSON.stringify(response));
+		// res.json(response);
+	})
+	.catch((error) => {
+		console.error(error);
+	});
 });
 
 module.exports = router;
